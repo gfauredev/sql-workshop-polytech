7e456af078771e46a99002b2ee23699467c5d250
Guilhem Fauré <pro@gfaure.eu>
2025-06-02 17:06:16 +0200

    feat: ex5 precisions

--- ex5.sql
+++ ex5.sql
@@ -1,10 +1,17 @@
-UPDATE materiel
-SET quantite = 5
+-- La quantité disponible n’est pas stockée dans la base, mais se calcule
+-- en soustrayant à la quantité possédée le nombre de réservations en cours.
+-- Modifier la quantité disponible signifie donc faire une réservation, mais
+-- pour utiliser UPDATE nous allons modifier la quantité possédée à la place.
+UPDATE materiel m
+SET quantite = m.quantite + 5 -- Ajout de 5 ordinateurs
 WHERE nom = 'Ordinateur Portable Dell XPS';
 
+-- On donne définitivement aux élèves le materiel qu’ils
+-- empruntent actuellement et ne devaient pas rendre avant au moins 2 jours
 UPDATE materiel m
 SET quantite = m.quantite - 1
 FROM reservation r
-WHERE m.id = r.materiel
+WHERE m.id = r.materiel -- jointure implicite
   AND r.statut = 'Approuvée'
-  AND r.fin > CURRENT_TIMESTAMP + INTERVAL '2 days';
+  AND r.fin > CURRENT_TIMESTAMP + INTERVAL '2 days'
+  AND m.quantite > 0; -- on évite les quantités négatives
